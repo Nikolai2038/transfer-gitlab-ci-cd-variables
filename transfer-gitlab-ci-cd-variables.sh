8c0e52a6b3d315531cc0996fdc634b32e21ade6f
Nikolai2038 <nikolai2038@yandex.ru>
2025-02-05 09:58:15 +0300

    Get project ID in separate function

--- transfer-gitlab-ci-cd-variables.sh
+++ transfer-gitlab-ci-cd-variables.sh
@@ -3,24 +3,7 @@
 # Fail command if any of pipeline blocks fail
 set -o pipefail || exit "$?"
 
-# Usage: transfer_gitlab_ci_cd_variables [--proxy <proxy_url>] <gitlab_project_1_url> <gitlab_project_1_api_token> <gitlab_project_2_url> <gitlab_project_2_api_token>
-function transfer_gitlab_ci_cd_variables() {
-  local proxy
-  if [ "${1}" == "--proxy" ]; then
-    proxy="${2}" && { shift || true; }
-    shift
-  fi
-
-  if [ "$#" -ne 4 ]; then
-    echo "Usage: ${0} [--proxy <proxy_url>] <gitlab_project_1_url> <gitlab_project_1_api_token> <gitlab_project_2_url> <gitlab_project_2_api_token>" >&2
-    return 1
-  fi
-
-  local gitlab_project_1_url="${1}" && { shift || true; }
-  local gitlab_project_1_api_token="${1}" && { shift || true; }
-  local gitlab_project_2_url="${1}" && { shift || true; }
-  local gitlab_project_2_api_token="${1}" && { shift || true; }
-
+function check_requirements() {
   if ! which curl &> /dev/null; then
     echo '"curl" is not installed!' >&2
     return 1
@@ -35,6 +18,68 @@ function transfer_gitlab_ci_cd_variables() {
     echo '"sed" is not installed!' >&2
     return 1
   fi
+}
+
+function get_project_id() {
+  if [ "$#" -lt 3 ]; then
+    echo "Usage: ${FUNCNAME[0]} <gitlab_hostname> <gitlab_project_url> <gitlab_api_token> [curl_arg]..." >&2
+    return 1
+  fi
+
+  local gitlab_hostname="${1}" && { shift || true; }
+  local gitlab_project_url="${1}" && { shift || true; }
+  local gitlab_api_token="${1}" && { shift || true; }
+
+  local project_name
+  project_name="${gitlab_project_url##*/}" || return "$?"
+  if [ -z "${project_name}" ]; then
+    echo "Failed to extract project name from the project URL!" >&2
+    return 1
+  fi
+
+  local projects_info
+  projects_info="$(curl --header "PRIVATE-TOKEN: ${gitlab_api_token}" "${@}" "${gitlab_hostname}/api/v4/projects?search=${project_name}")" || return "$?"
+  if [ -z "${projects_info}" ]; then
+    echo "Failed to get projects info!" >&2
+    return 1
+  fi
+
+  local project_info
+  project_info="$(echo "${projects_info}" | jq -r ".[] | select(.web_url == \"${gitlab_project_url}\")")" || return "$?"
+  if [ -z "${project_info}" ]; then
+    echo "Failed to get project info!" >&2
+    return 1
+  fi
+
+  local project_id
+  project_id="$(echo "${project_info}" | jq -r '.id')" || return "$?"
+  if [ -z "${project_id}" ]; then
+    echo "Failed to extract project ID from the project info!" >&2
+    return 1
+  fi
+
+  echo "${project_id}"
+}
+
+# Usage: transfer_gitlab_ci_cd_variables [--proxy <proxy_url>] <gitlab_project_1_url> <gitlab_project_1_api_token> <gitlab_project_2_url> <gitlab_project_2_api_token>
+function transfer_gitlab_ci_cd_variables() {
+  local proxy
+  if [ "${1}" == "--proxy" ]; then
+    shift || true
+    proxy="${1}" && { shift || true; }
+  fi
+
+  if [ "$#" -ne 4 ]; then
+    echo "Usage: ${FUNCNAME[0]} [--proxy <proxy_url>] <gitlab_project_1_url> <gitlab_project_1_api_token> <gitlab_project_2_url> <gitlab_project_2_api_token>" >&2
+    return 1
+  fi
+
+  local gitlab_project_1_url="${1}" && { shift || true; }
+  local gitlab_project_1_api_token="${1}" && { shift || true; }
+  local gitlab_project_2_url="${1}" && { shift || true; }
+  local gitlab_project_2_api_token="${1}" && { shift || true; }
+
+  check_requirements || return "$?"
 
   # Arguments for "curl" commands
   declare -a curl_extra_args=(
@@ -65,61 +110,25 @@ function transfer_gitlab_ci_cd_variables() {
   )
 
   local gitlab_hostname_1
-  gitlab_hostname_1="$(echo "${gitlab_project_1_url}" | sed -r 's#^https?://([^/]+)/.*$#\1#')" || return "$?"
+  gitlab_hostname_1="$(echo "${gitlab_project_1_url}" | sed -E 's#^(https?://[^/]+)/.*$#\1#')" || return "$?"
   if [ -z "${gitlab_hostname_1}" ]; then
     echo "Failed to extract GitLab hostname from \"${gitlab_project_1_url}\"!" >&2
     return 1
   fi
 
   local gitlab_hostname_2
-  gitlab_hostname_2="$(echo "${gitlab_project_2_url}" | sed -r 's#^https?://([^/]+)/.*$#\1#')" || return "$?"
-  if [ -z "${gitlab_hostname_1}" ]; then
+  gitlab_hostname_2="$(echo "${gitlab_project_2_url}" | sed -E 's#^(https?://[^/]+)/.*$#\1#')" || return "$?"
+  if [ -z "${gitlab_hostname_2}" ]; then
     echo "Failed to extract GitLab hostname from \"${gitlab_project_2_url}\"!" >&2
     return 1
   fi
 
-  local project_1_name
-  project_1_name="${gitlab_project_1_url##*/}" || return "$?"
-  echo "Project 1 name: ${project_1_name}"
-
-  local project_2_name
-  project_2_name="${gitlab_project_2_url##*/}" || return "$?"
-  echo "Project 2 name: ${project_2_name}"
-
-  local projects_info_1
-  projects_info_1="$(curl "${curl_extra_args_for_gitlab_1[@]}" "https://${gitlab_hostname_1}/api/v4/projects?search=${project_1_name}")" || return "$?"
-
-  local projects_info_2
-  projects_info_2="$(curl "${curl_extra_args_for_gitlab_2[@]}" "https://${gitlab_hostname_2}/api/v4/projects?search=${project_2_name}")" || return "$?"
-
-  local project_info_1
-  project_info_1="$(echo "${projects_info_1}" | jq -r ".[] | select(.web_url == \"${gitlab_project_1_url}\")")" || return "$?"
-  # DEBUG:
-  # echo "Project 1 info: ${projects_info_1}"
-
-  local project_info_2
-  project_info_2="$(echo "${projects_info_2}" | jq -r ".[] | select(.web_url == \"${gitlab_project_2_url}\")")" || return "$?"
-  # DEBUG:
-  # echo "Project 2 info: ${projects_info_2}"
-
   local project_id_1
-  project_id_1="$(echo "${project_info_1}" | jq -r '.id')" || return "$?"
-  if [ -z "${project_id_1}" ]; then
-    echo "Failed to extract project 1 ID from the project info!" >&2
-    return 1
-  fi
-
-  # DEBUG:
+  project_id_1="$(get_project_id "${gitlab_hostname_1}" "${gitlab_project_1_url}" "${gitlab_project_1_api_token}" "${curl_extra_args[@]}")" || return "$?"
   echo "Project 1 ID: ${project_id_1}"
 
   local project_id_2
-  project_id_2="$(echo "${project_info_2}" | jq -r '.id')" || return "$?"
-  if [ -z "${project_id_2}" ]; then
-    echo "Failed to extract project 2 ID from the project info!" >&2
-    return 1
-  fi
-
-  # DEBUG:
+  project_id_2="$(get_project_id "${gitlab_hostname_2}" "${gitlab_project_2_url}" "${gitlab_project_2_api_token}" "${curl_extra_args[@]}")" || return "$?"
   echo "Project 2 ID: ${project_id_2}"
 
   local is_all_pages_parsed=0
@@ -131,7 +140,7 @@ function transfer_gitlab_ci_cd_variables() {
 
   while ((!is_all_pages_parsed)); do
     local project_api_1
-    project_api_1="https://${gitlab_hostname_1}/api/v4/projects/${project_id_1}/variables?per_page=${per_page}&page=${page}"
+    project_api_1="${gitlab_hostname_1}/api/v4/projects/${project_id_1}/variables?per_page=${per_page}&page=${page}"
 
     local project_1_headers_filename="/tmp/project_1_headers.txt"
 
@@ -185,13 +194,13 @@ function transfer_gitlab_ci_cd_variables() {
       variable_description="$(echo "${variable}" | jq -r '(.description // "")')" || return "$?"
 
       # Check if variable exists
-      if curl "${curl_extra_args_for_gitlab_2[@]}" --head "https://${gitlab_hostname_2}/api/v4/projects/${project_id_2}/variables/${variable_key}?filter%5Benvironment_scope%5D=${variable_environment_scope}" &> /dev/null; then
+      if curl "${curl_extra_args_for_gitlab_2[@]}" --head "${gitlab_hostname_2}/api/v4/projects/${project_id_2}/variables/${variable_key}?filter%5Benvironment_scope%5D=${variable_environment_scope}" &> /dev/null; then
         echo "Variable \"${variable_key}\" already exists in the second project for scope \"${variable_environment_scope}\" - skipping it" >&2
         continue
       fi
 
       # Create variable
-      curl "${curl_extra_args_for_gitlab_2[@]}" --request POST "https://${gitlab_hostname_2}/api/v4/projects/${project_id_2}/variables" \
+      curl "${curl_extra_args_for_gitlab_2[@]}" --request POST "${gitlab_hostname_2}/api/v4/projects/${project_id_2}/variables" \
         --form "key=${variable_key}" \
         --form "value=${variable_value}" \
         --form "protected=${variable_protected}" \
